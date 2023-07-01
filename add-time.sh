4b4ae556a6e9dfd4bae2a2b25c80a125754b15de
pschlo <>
2023-07-01 18:09:31 +0200

    improved comments

--- add-time.sh
+++ add-time.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
+# this script adds a timestamp to lines from STDIN
+
 
 # by default, the script should not terminate from signals
+# otherwise, writing pipe end might receive SIGPIPE
 for ((i=0; i<100; i++)); do
     trap : $i 2>/dev/null && true
 done
 
-# append date to input
+# add timestamp to input
 while IFS= read -r line; do
     echo "[$(date +"%Y-%m-%d %T")] $line"
 done
